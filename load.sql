79dfd71ebba5366e615a2f7814d87d77a8b1a58c
Valerie Liang <143350210+valerieliang@users.noreply.github.com>
2026-04-05 23:09:02 -0400

    make load prettier

--- load.sql
+++ load.sql
@@ -1,5 +1,7 @@
 -- ---------------------------------------------------------------
 -- 1. NEOs
+-- NOT NULL cols: id, name, potentially_hazardous, num_satellites
+-- No nullable cols → no SET clause needed
 -- ---------------------------------------------------------------
 LOAD DATA LOCAL INFILE './data/neos.csv'
 INTO TABLE NEOs
@@ -11,6 +13,10 @@ IGNORE 1 LINES
 
 -- ---------------------------------------------------------------
 -- 2. PhysicalInfo
+-- NOT NULL: id
+-- NULLABLE: diameter, absolute_magnitude, slope_parameter, albedo,
+--           rotational_period_hrs, gravitational_param,
+--           spectral_class_b, spectral_class_t
 -- ---------------------------------------------------------------
 LOAD DATA LOCAL INFILE './data/physical_info.csv'
 INTO TABLE PhysicalInfo
@@ -18,11 +24,25 @@ FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(id, diameter, absolute_magnitude, slope_parameter, albedo, 
- rotational_period_hrs, gravitational_param, spectral_class_b, spectral_class_t);
+(id,
+ @diameter, @absolute_magnitude, @slope_parameter, @albedo,
+ @rotational_period_hrs, @gravitational_param,
+ @spectral_class_b, @spectral_class_t)
+SET
+    diameter              = NULLIF(TRIM(@diameter),              ''),
+    absolute_magnitude    = NULLIF(TRIM(@absolute_magnitude),    ''),
+    slope_parameter       = NULLIF(TRIM(@slope_parameter),       ''),
+    albedo                = NULLIF(TRIM(@albedo),                ''),
+    rotational_period_hrs = NULLIF(TRIM(@rotational_period_hrs), ''),
+    gravitational_param   = NULLIF(TRIM(@gravitational_param),   ''),
+    spectral_class_b      = NULLIF(TRIM(@spectral_class_b),      ''),
+    spectral_class_t      = NULLIF(TRIM(@spectral_class_t),      '');
 
 -- ---------------------------------------------------------------
 -- 3. OrbitalData
+-- NOT NULL: id
+-- NULLABLE: eccentricity, inclination, orbital_period,
+--           orbit_class, earth_moid
 -- ---------------------------------------------------------------
 LOAD DATA LOCAL INFILE './data/orbital_data.csv'
 INTO TABLE OrbitalData
@@ -30,10 +50,21 @@ FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(id, eccentricity, inclination, orbital_period, orbit_class, earth_moid);
+(id,
+ @eccentricity, @inclination, @orbital_period,
+ @orbit_class, @earth_moid)
+SET
+    eccentricity   = NULLIF(TRIM(@eccentricity),   ''),
+    inclination    = NULLIF(TRIM(@inclination),    ''),
+    orbital_period = NULLIF(TRIM(@orbital_period), ''),
+    orbit_class    = NULLIF(TRIM(@orbit_class),    ''),
+    earth_moid     = NULLIF(TRIM(@earth_moid),     '');
 
 -- ---------------------------------------------------------------
 -- 4. ObservationRecord
+-- NOT NULL: observation_id (AUTO_INCREMENT, not in column list),
+--           id
+-- NULLABLE: discovering_org, first_obs, latest_obs, num_obs
 -- ---------------------------------------------------------------
 LOAD DATA LOCAL INFILE './data/observation_records.csv'
 INTO TABLE ObservationRecord
@@ -41,10 +72,19 @@ FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(observation_id, id, discovering_org, first_obs, latest_obs, num_obs);
+(observation_id, id,
+ @discovering_org, @first_obs, @latest_obs, @num_obs)
+SET
+    discovering_org = NULLIF(TRIM(@discovering_org), ''),
+    first_obs       = NULLIF(TRIM(@first_obs),       ''),
+    latest_obs      = NULLIF(TRIM(@latest_obs),      ''),
+    num_obs         = NULLIF(TRIM(@num_obs),         '');
 
 -- ---------------------------------------------------------------
 -- 5. CloseApproaches
+-- NOT NULL: approach_id (AUTO_INCREMENT, not in column list),
+--           id, close_approach_date
+-- NULLABLE: miss_distance, relative_velocity, v_infinity
 -- ---------------------------------------------------------------
 LOAD DATA LOCAL INFILE './data/close_approaches.csv'
 INTO TABLE CloseApproaches
@@ -52,13 +92,9 @@ FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
-(approach_id, id, close_approach_date, miss_distance, relative_velocity, v_infinity);
-
--- ---------------------------------------------------------------
--- Row counts
--- ---------------------------------------------------------------
-SELECT 'NEOs' AS tbl, COUNT(*) FROM NEOs UNION ALL
-SELECT 'PhysicalInfo', COUNT(*) FROM PhysicalInfo UNION ALL
-SELECT 'OrbitalData', COUNT(*) FROM OrbitalData UNION ALL
-SELECT 'CloseApproaches', COUNT(*) FROM CloseApproaches UNION ALL
-SELECT 'ObservationRecord', COUNT(*) FROM ObservationRecord;
+(approach_id, id, close_approach_date,
+ @miss_distance, @relative_velocity, @v_infinity)
+SET
+    miss_distance     = NULLIF(TRIM(@miss_distance),     ''),
+    relative_velocity = NULLIF(TRIM(@relative_velocity), ''),
+    v_infinity        = NULLIF(TRIM(@v_infinity),        '');
