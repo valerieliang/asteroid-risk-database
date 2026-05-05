accc0df70cdc1fdad38ef3a06bd4b242ace760cc
Valerie Liang <vvalerieliang@gmail.com>
2026-05-05 13:53:55 -0400

    edit file names

--- load.sql
+++ load.sql
@@ -29,7 +29,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS (spkid, data_arc, condition_code);
 
 -- Load predictions from Random Forest inference 
-LOAD DATA LOCAL INFILE 'data/predictions_fixed.csv'
+LOAD DATA LOCAL INFILE 'data/predictions_unlabelled.csv'
 INTO TABLE PredictedPHAs
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
@@ -38,7 +38,7 @@ IGNORE 1 ROWS
 (spkid, pred_pha, pha_prob);
 
 -- Load predicted anomaly flags from anomaly flagger
-LOAD DATA LOCAL INFILE 'data/anomalies_fixed.csv'
+LOAD DATA LOCAL INFILE 'data/anomalies_only.csv'
 INTO TABLE OrbitalAnomalies
 FIELDS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
